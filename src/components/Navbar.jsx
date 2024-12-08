ea9c570275332572f5554eee8743e3d49b3ffe44
bhargavi <bhargavikarankote@gmail.com>
2024-12-08 19:11:30 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,8 @@
 
-// import { Link } from 'react-router-dom';
-
-// import React from "react";
-// import "../styles/Navbar.css";
-
-// function Navbar() {
-//   return (
-//     <nav className="navbar">
-//       <div className="logo-container">
-//         <img
-//           src="https://swarainfra.homes/static/media/logo.b9aec6b0bfc6345fdc2e.png"
-//           alt="Swara Infra Logo"
-//           className="navbar-logo"
-//         />
-//         <div className="logo-text">Swara Infra</div>
-//       </div>
-
-//         <ul className="nav-links" >
-//           <li><Link to="/">Home</Link></li>
-//           <li><Link to="/about">About</Link></li>
-//           <li><Link to="/contact">Contact</Link></li>
-//           <li><Link to="/projects">Projects</Link></li>
-//           <li><Link to="/Packages">Packages</Link></li>
-
-//         </ul>
-
-
-
-
-      
-//     </nav>
-//   );
-// }
 
 // export default Navbar;
+// 
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
@@ -43,6 +12,10 @@ function Navbar() {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
   };
 
   return (
@@ -63,11 +36,11 @@ function Navbar() {
       </div>
       {/* Navigation Links */}
       <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/packages">Packages</Link></li>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
+        <li><Link to="/projects" onClick={closeMenu}>Projects</Link></li>
+        <li><Link to="/packages" onClick={closeMenu}>Packages</Link></li>
       </ul>
     </nav>
   );
